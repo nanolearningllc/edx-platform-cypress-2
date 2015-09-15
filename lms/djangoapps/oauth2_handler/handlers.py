singhularity/edx-platform

Optimize OpenID Course Claims for users. - Course overviews will cache courses upon publish. - Added management command to warm up cache. - OAuth2 handler returns courses via course overviews.

lms/djangoapps/oauth2_handler/handlers.py
@@ -2,9 +2,9 @@
 
 from django.conf import settings
 from django.core.cache import cache
-from xmodule.modulestore.django import modulestore
 
 from courseware.access import has_access
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from openedx.core.djangoapps.user_api.models import UserPreference
 from student.models import anonymous_id_for_user
 from student.models import UserProfile
@@ -200,13 +200,13 @@ class CourseAccessHandler(object):
         course_ids = cache.get(key)
 
         if not course_ids:
-            courses = _get_all_courses()
+            courses = CourseOverview.get_all_courses()
 
             # Global staff have access to all courses. Filter courses for non-global staff.
             if not GlobalStaff().has_user(user):
                 courses = [course for course in courses if has_access(user, access_type, course)]
 
-            course_ids = [unicode(course.id) for course in courses]
+            course_ids = [unicode(course) for course in courses]
 
             cache.set(key, course_ids, self.COURSE_CACHE_TIMEOUT)
 
@@ -234,12 +234,3 @@ class IDTokenHandler(OpenIDHandler, ProfileHandler, CourseAccessHandler, Permiss
 class UserInfoHandler(OpenIDHandler, ProfileHandler, CourseAccessHandler, PermissionsHandler):
     """ Configure the UserInfo handler for the LMS. """
     pass
-
-
-def _get_all_courses():
-    """ Utility function to list all available courses. """
-
-    ms_courses = modulestore().get_courses()
-    courses = [course for course in ms_courses if course.scope_ids.block_type == 'course']
-
-    return courses

nanolearningllc/edx-platform-cypress-2

lms/djangoapps/oauth2_handler/handlers.py
@@ -2,9 +2,9 @@
 
 from django.conf import settings
 from django.core.cache import cache
-from xmodule.modulestore.django import modulestore
 
 from courseware.access import has_access
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from openedx.core.djangoapps.user_api.models import UserPreference
 from student.models import anonymous_id_for_user
 from student.models import UserProfile
@@ -200,13 +200,13 @@ class CourseAccessHandler(object):
         course_ids = cache.get(key)
 
         if not course_ids:
-            courses = _get_all_courses()
+            courses = CourseOverview.get_all_course_keys()
 
             # Global staff have access to all courses. Filter courses for non-global staff.
             if not GlobalStaff().has_user(user):
                 courses = [course for course in courses if has_access(user, access_type, course)]
 
-            course_ids = [unicode(course.id) for course in courses]
+            course_ids = [unicode(course) for course in courses]
 
             cache.set(key, course_ids, self.COURSE_CACHE_TIMEOUT)
 
@@ -234,12 +234,3 @@ class IDTokenHandler(OpenIDHandler, ProfileHandler, CourseAccessHandler, Permiss
 class UserInfoHandler(OpenIDHandler, ProfileHandler, CourseAccessHandler, PermissionsHandler):
     """ Configure the UserInfo handler for the LMS. """
     pass
-
-
-def _get_all_courses():
-    """ Utility function to list all available courses. """
-
-    ms_courses = modulestore().get_courses()
-    courses = [course for course in ms_courses if course.scope_ids.block_type == 'course']
-
-    return courses